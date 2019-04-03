999f323a661a620493f27308f607ef3022ba2c51
Antonio López <lopeaa@gmail.com>
2019-04-03 10:39:16 +0200

    v.1.0

--- app/Http/Controllers/RootCRLController.php
+++ app/Http/Controllers/RootCRLController.php
@@ -18,65 +18,56 @@ class RootCRLController extends Controller
         $this->middleware('auth');
     }
 
-
   	public function root()
    	{
-        return view ('rootcrl.root');     
+        return view ('rootcrl.root');
     }
 
     public function getRootTRAGSA()
     {
       $headers = array('Content_Type: application/x-download',);
-      return Response::download(storage_path('cert.ca.cer'), 'cert.ca.cer', $headers)->deleteFileAfterSend(true);
-
+      //return Response::download(storage_path('cert.ca.cer'), 'cert.ca.cer', $headers)->deleteFileAfterSend(true);
+      return Response::download(storage_path('cert.ca.cer'), 'root.cert.pem', $headers)->deleteFileAfterSend(true);
     }
 
     public function getRootLE()
     {
       $headers = array('Content_Type: application/x-download',);
       return Response::download(storage_path('le.ca.cer'), 'le.ca.cer', $headers)->deleteFileAfterSend(true);
-
     }
-
 
     public function crl()
     {
-        return view ('rootcrl.crl');     
-
+        return view ('rootcrl.crl');
     }
-
 
     public function updateCRL(Request $request)
     {
-        
+
         $password = $request::input('password');
         $crlPath = storage_path('ca-g2.crl');
-
         $updateCRL = shell_exec("sudo openssl ca -config /opt/CA/openssl.cnf -gencrl -out $crlPath -key $password -batch 2>&1");
-
 
         return view ('rootcrl.updateCRL', array(
             'result' => $updateCRL, )
-        );     
-
+        );
     }
 
     public function getCRL()
     {
       $headers = array('Content_Type: application/x-download',);
       return Response::download(storage_path('ca-g2.crl'), 'ca-g2.crl', $headers)->deleteFileAfterSend(true);
-
     }
 
     public function authenticode()
    	{
 
             return view ('signer.authenticode');
-    }   
+    }
 
         public function signAuthenticode(Request $request)
     {
-        if ($request::hasFile('archive')) 
+        if ($request::hasFile('archive'))
     	{
         $storagePath = storage_path();
         $archive = $request::file('archive');
@@ -89,7 +80,7 @@ class RootCRLController extends Controller
         // Variables to exec jarsigner.
 		$keystore = "/opt/keystore/symantec_cs.p12";
 		$keystorealias = "grupotragsacs";
-		$tsaurl = "http://sha256timestamp.ws.symantec.com/sha256/timestamp"; // Timestamp Server used by Symantec. 
+		$tsaurl = "http://sha256timestamp.ws.symantec.com/sha256/timestamp"; // Timestamp Server used by Symantec.
 
         $osslsigncode = shell_exec("osslsigncode sign -pkcs12 $keystore -pass $password -h sha2 -in $archive_uploaded -out $storagePath/$archive_name.signed 2>&1");
 
@@ -99,13 +90,13 @@ class RootCRLController extends Controller
             'archive_name' => $archive_name,
             'archive_type' => $archive_type,
             'result' => $osslsigncode )
-        );     
+        );
 
     }
-     
+
     public function getAuthenticode(Request $request)
    {
-      
+
           $archive_name = $request::input('archive_name');
           $archive_type = $request::input('archive_type');
 
@@ -113,7 +104,7 @@ class RootCRLController extends Controller
           //Cert::where('dstalias', $dstalias)->update(['keystore' => $dstalias]);
 
           $headers = array('Content_Type: application/x-download',);
-          
+
         return Response::download(storage_path($archive_name . '.signed'), $archive_name . $archive_type, $headers)->deleteFileAfterSend(true);
 
    }
