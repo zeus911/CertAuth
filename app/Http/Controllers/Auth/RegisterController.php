c133f192cbfb4acf146be073cdc866f2ddabe227
lopeaa <lopeaa@gmail.com>
2019-04-03 19:08:23 +0200

    v.1.0

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'certs/mgmt';
 
     /**
      * Create a new controller instance.
