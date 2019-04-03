d58368b42ff228eccf245b5407bf881134aece0e
Antonio López <lopeaa@gmail.com>
2019-04-03 11:41:09 +0200

    v.1.0

--- app/Http/Controllers/RootCRLController.php
+++ app/Http/Controllers/RootCRLController.php
@@ -46,7 +46,7 @@ class RootCRLController extends Controller
 
         $password = $request::input('password');
         $crlPath = storage_path('root.ca.crl');
-        $updateCRL = shell_exec("sudo openssl ca -config /opt/CA/openssl.cnf -gencrl -out $crlPath -key $password -batch 2>&1");
+        $updateCRL = shell_exec("sudo openssl ca -config /etc/ssl/openssl.cnf -gencrl -out $crlPath -key $password -batch 2>&1");
 
         return view ('rootcrl.updateCRL', array(
             'result' => $updateCRL, )
