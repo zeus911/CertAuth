fc0f106ace38edb0d6f2a13244a520b0985fadc8
Antonio López <lopeaa@gmail.com>
2019-04-09 12:45:15 +0200

    v.1.0

--- resources/views/certs/mgmt/updated.blade.php
+++ resources/views/certs/mgmt/updated.blade.php
@@ -4,12 +4,12 @@
 
 <div class="container">
 
-      <h2 class="text-primary">You have updated the CSR/Certificate OR Private Key of:<strong class="text-success"> {{ $subjectCommonName }}</strong>.</h2>
+      <h2 class="">You have updated the CSR/Certificate OR Private Key of:<strong class="text-success"> {{ $subjectCommonName }}</strong>.</h2>
       <br />
       {{ Form::open(['url' => 'certs/mgmt', 'files' => 'true', 'method' => 'get']) }}
       {{ Form::token() }}
-      {{ Form::submit('Go back to Managemnet', ['class' => 'btn btn-outline-primary']) }}
-      {{ Form::close() }}  
+      {{ Form::submit('Go back to Managemnet', ['class' => 'btn btn-secondary']) }}
+      {{ Form::close() }}
       <br />
 </div>
 @endsection
