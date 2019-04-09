bce796c13f332c7e6b10f22440d364ee5170aa6d
Antonio López <lopeaa@gmail.com>
2019-04-09 13:44:27 +0200

    v.1.0

--- resources/views/signer/signJAR.blade.php
+++ resources/views/signer/signJAR.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 
 <div class="container">
-    <H2>Your archive {{ $jarName }} has been:</H2>
-    <H3 class="">{{ $result }}</H3>
+        <H2>Your archive {{ $jarName }} has been:</H2>
+    <div class="alert alert-primary" role="alert">
+        <H3 class="">{{ $result }}</H3>
+    </div>
     <div class="container">
         {{ Form::open(['url' => 'signer/getJAR', 'method' => 'post']) }}
         <input type="hidden" name="jarName" value="{{ $jarName }}">
         {{ Form::token() }}
         {{ Form::submit('Get Signed JAR', ['class' => 'btn btn-primary']) }}
         {{ Form::close() }}
-
     </div>
 
 @endsection
