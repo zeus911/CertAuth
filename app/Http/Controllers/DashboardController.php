09b6ba90d37853e097666dfca5eac427b9b08e85
Antonio López <lopeaa@gmail.com>
2019-04-09 13:10:42 +0200

    v.1.0

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index()
     {
         // All Certs collections.
@@ -32,19 +32,19 @@ class DashboardController extends Controller
         $certs_number_issued->dataset('Nº of Certificates Created In', 'bar', [$certs_year, $certs_month])->color(['#0080ff', '#00bfff']);
 
         // Chart - Certificates status.
-        $certs_status_blank = Cert::where('status', '=', '')->count();      
+        $certs_status_blank = Cert::where('status', '=', '')->count();
         $certs_status_valid = Cert::where('status', '=', 'Valid')->count();
-        $certs_status_expiring = Cert::where('status', '=', 'Expiring')->count();      
+        $certs_status_expiring = Cert::where('status', '=', 'Expiring')->count();
         $certs_status_expired = Cert::where('status', '=', 'Expired')->count();
         $certs_status_revoked = Cert::where('status', '=', 'Revoked')->count();
         $certs_status = new CertsStatus;
         $certs_status->labels(['N/A', 'Valid', 'Expiring', 'Expired', 'Revoked']);
         $certs_status->dataset('Certificates Status', 'bar', [$certs_status_blank, $certs_status_valid, $certs_status_expiring, $certs_status_expired, $certs_status_revoked])
         ->color(['#3333ff', '#33cc33', '#ff8000', '#ff0000', '#ff0000']);
-        //$certs_status->container($certs_status->lebels = null); 
- 
+        //$certs_status->container($certs_status->lebels = null);
+
         // Charts Issued by CA
-        $IssuerCNTR = Cert::where('issuerCN', 'like', '%TRAGSA%')->count();
+        $IssuerCNLB = Cert::where('issuerCN', 'like', '%LIQUAB%')->count();
         $IssuerCNFP = Cert::where('issuerCN', 'like', '%FIRMAPROFESIONAL%')->count();
         $IssuerCNDC = Cert::where('issuerCN', 'like', '%DigiCert%')->count();
         $IssuerCNSY = Cert::where('issuerCN', 'like', '%Symantec%')->count();
@@ -54,7 +54,7 @@ class DashboardController extends Controller
 
         $certs_issued_by = new CertsIssuedBy;
         $certs_issued_by->labels(['TRAGSA CA G2', 'Firma Profesional', 'DigiCert', 'Symantec', 'Verisign', "Let's Encrypt", "DST Root CA X3"]);
-        $certs_issued_by->dataset('Nº of Issued By each CA', 'bar', [$IssuerCNTR, $IssuerCNFP, $IssuerCNDC, $IssuerCNSY, $IssuerCNVE, $IssuerCNLE1, $IssuerCNLE2 ])
+        $certs_issued_by->dataset('Nº of Issued By each CA', 'bar', [$IssuerCNLB, $IssuerCNFP, $IssuerCNDC, $IssuerCNSY, $IssuerCNVE, $IssuerCNLE1, $IssuerCNLE2 ])
         ->color(['#ff8000', '#3333ff', '#ff8000', '#3333ff', '#ff8000', '#3333ff']);
 
         // Charts - Certificate types.
@@ -69,10 +69,10 @@ class DashboardController extends Controller
 //dd($certs_tls_web_server, $certs_cliencerts_tls_web_client, $certs_code_signing);
         return view ('dashboard.index', [
           'certsNumber' => $certsNumber,
-          'certs_number_issued' => $certs_number_issued, 
+          'certs_number_issued' => $certs_number_issued,
           'certs_status' => $certs_status,
           'certs_issued_by' => $certs_issued_by,
           'certs_type' => $certs_type ]);
-       }    
+       }
     }
 
