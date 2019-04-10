3a19698793414fac4c65423ffffb51fa4681c126
Antonio López <lopeaa@gmail.com>
2019-04-10 14:51:50 +0200

    v.1.0

--- resources/views/signer/signAuthenticode.blade.php
+++ resources/views/signer/signAuthenticode.blade.php
@@ -3,14 +3,17 @@
 @section('content')
 
 <div class="container">
-    <H2>Signature for {{ $archive_name }} has {{ $result }}.</H2>
-    <div class="container">
+    <h2>Signature for {{ $archive_name }} has:<h2>
+        <div class="alert alert-light" role="alert">
+            {{ $result }}
+        </div>
+                <div class="container">
         {{ Form::open(['url' => 'signer/getAuthenticode', 'method' => 'post']) }}
         <input type="hidden" name="archive_name" value="{{ $archive_name }}">
         {{ Form::token() }}
         {{ Form::submit('Get Signed Archive', ['class' => 'btn btn-primary btn-md']) }}
         {{ Form::close() }}
-            
+
     </div>
 
 @endsection
