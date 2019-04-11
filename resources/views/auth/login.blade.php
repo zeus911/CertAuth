961f4b8230380c98c63114c92ef19c7ac5ba7b1f
Antonio López <lopeaa@gmail.com>
2019-04-11 11:54:08 +0200

    v.1.0

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                             </div>
 
 
-                        <div class="form-group row">
+{{--                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -65,7 +65,7 @@
                                 </div>
                             </div>
                         </div>
-
+ --}}
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
