9b9daf88a5e97cd5a72cb2595393d4f1cbbede82
Antonio López <lopeaa@gmail.com>
2019-04-11 12:02:56 +0200

    v.1.0

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -57,7 +57,7 @@
     <!-- footer -->
 
     <div class="container">
-        <div class="text-info"><center><strong><a href="https://prototypes.liquait.com"<i class="fab fa-product-hunt"></i>rototypes} </a>, 2016 - {{ date('F Y') }}.</strong></center></div>
+        <div class="text-primary"><center><strong><a href="https://prototypes.liquait.com" <i class="fab fa-product-hunt"></i>rototypes, 2016 - {{ date('F Y') }}.</a></strong></center></div>
         <div class="text-muted"><center><strong><i class="fa fa-quote-left" aria-hidden="true"></i> Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning...<i class="fa fa-quote-right" aria-hidden="true"></i></strong></center></div>
     </div>
 </div>
