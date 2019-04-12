3ba0dc90b04569e844739fec8a4d4d1a24aaace4
Antonio López <lopeaa@gmail.com>
2019-04-12 12:41:20 +0200

    v.1.0

--- resources/views/signer/jar.blade.php
+++ resources/views/signer/jar.blade.php
@@ -3,13 +3,15 @@
 @section('content')
 
 <div class="container">
-    <H2>Sign a Java Archive (JAR).</H2>
+    <H2>Sign a Java Archive (JAR)</H2>
     <div class="container">
 
     {{ Form::open(['url' => 'signer/signJAR', 'method' => 'POST', 'class' => 'form', 'files' => true]) }}
         <div class="form-group">
-            {{ Form::label('Select JAR Archive') }}
-            {{ Form::file('jar', null, ['class' => 'form-control']) }}
+            {{--  {{ Form::label('Select JAR Archive') }}  --}}
+            {{--  {{ Form::file('jar', null, ['class' => 'form-control']) }}  --}}
+            {{ Form::file('jar[]', null, ["class"=>"required","multiple"=>true]) }}
+
         </div>
 
         <div class="form-group">
