f43fea44796a0c2471f55fc16945e08b6936e983
Antonio López <lopeaa@gmail.com>
2019-04-12 13:52:51 +0200

    v.1.0

--- resources/views/signer/authenticode.blade.php
+++ resources/views/signer/authenticode.blade.php
@@ -5,21 +5,21 @@
 <div class="container">
     <H2>Sign a Microsoft Archive.</H2>
     <div class="container">
-
+    <br />
     {{ Form::open(['url' => 'signer/signAuthenticode', 'method' => 'POST', 'class' => 'form', 'files' => true]) }}
 		<div class="form-group">
-		    {{ Form::label('Select Microsoft Archive') }}
+		    {{--  {{ Form::label('Select Microsoft Archive') }}  --}}
 		    {{ Form::file('archive', null) }}
 		</div>
 		<div class="form-group">
-			{{ Form::label('Archive Type: ', 'Archive Type: ', ['class' => '']) }}
+			{{--  {{ Form::label('Archive Type: ', 'Archive Type: ', ['class' => '']) }}  --}}
         	{{ Form::select('archive_type', ['.msi' => 'MSI', '.exe' => 'EXE', '.dll' => 'DLL', '.cab' => 'CAB'], null, ['placeholder' => 'Microsoft Archive Type', 'class' => 'form-control' ]) }}
         	@if($errors->has('archive_type'))
             {{ $errors->first('archive_type') }}
         	@endif
         <div>
         <div class="form-group">
-            {{ Form::label('Password') }}
+            {{--  {{ Form::label('Password') }}  --}}
             {{ Form::password('password', ['placeholder' => 'Keystore Password', 'class' => 'form-control' ]) }}
         </div>
 
