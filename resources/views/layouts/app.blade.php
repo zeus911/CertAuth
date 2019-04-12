f1e6e0f4fcd138795aa0b7256af16239d523d2f9
Antonio López <lopeaa@gmail.com>
2019-04-12 14:46:01 +0200

    v.1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,7 +145,25 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                            <li class="nav-item dropdown">
+                                    <a id="login" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <strong><i class="fas fa-user-astronaut"></i> {{ Auth::user()->name }} <span class="caret"></span></strong>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="login">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            <strong><i class="fas fa-sign-out-alt"></i> {{ __('User Profile') }}</strong>
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+
+                            @endguest
                     </ul>
                 </div>
             </div>
