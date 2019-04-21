84ee7811bf77b1ac1a95c0511611cc2d6360fb6f
lopeaa <lopeaa@gmail.com>
2019-04-21 16:06:42 +0200

    v.1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('certs/mgmt') }}">
                     {{--  <strong>{{ config('app.name', '') }} <span class="badge badge-light">PoC</span></strong>  --}}
-                    <div><img src="{{URL::asset('/svg/logo_ca_def.png')}}" alt="LIQUABit - Home"></div>
+                    <div><img src="{{URL::asset('/svg/logo_ca_sm.png')}}" alt="LIQUABit - Home"></div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
